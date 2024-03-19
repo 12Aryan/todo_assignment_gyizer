518473f56f7e2b7d13f074d31a7bfe900d8e96aa
12Aryan <86924503+12Aryan@users.noreply.github.com>
2024-03-19 20:28:53 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + Vite
+
+This app is created using vite
+To run this app use command: 
+1. npm i --f
+2. npm run dev
---
+++ src/redux/TodoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todoSlice",
+  initialState: initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+    },
+    updateTodo: (state, action) => {
+        const {updatedDataIndex, updateTodoData} = action.payload
+        console.log(updatedDataIndex, updateTodoData);
+        state.todos[updatedDataIndex] = updateTodoData
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter(
+        (item, index) => index !== action.payload
+      );
+    },
+  },
+});
+
+export default todoSlice.reducer;
+export const { addTodo, updateTodo, deleteTodo } = todoSlice.actions;
+
+export const getTodos = (state) => state.todoSlice.todos;
